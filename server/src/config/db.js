b8ad87d217e4d5a7fe33331ff94e4dada1db170b
Rishabh <rc4556c@gmail.com>
2024-03-23 21:15:50 +0530

    databse

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.DATABASE)
         console.log('Database Connected')
     } catch (error) {
         console.log('Database Error:', error)
