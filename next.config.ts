4be5ac8eb89ba6191ac22ebf5fcd063b89755c1f
Rishabh <rc4556c@gmail.com>
2026-09-24 22:09:52 +0530

    refactor: remove prisma and pg from serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   serverExternalPackages: ["@prisma/orm-postgres", "pg"],
    turbopack: {
       root: process.cwd(),
    },
